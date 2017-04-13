305f3d3a1af65ad1130b1435f47cc78bd3a077b4
sida7med <sida7medess@gmail.com>
2017-04-13 11:37:39 +0100

    Update and rename demande.html to demande.php

--- demande.php
+++ demande.php
@@ -2,9 +2,9 @@
 <html>
 <head>
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="boo/css/bootstrap.css">
-	<script src="boo/js/jquery.js"></script> 
-	<script src="boo/js/bootstrap.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<script src="js/jquery.min.js"></script> 
+	<script src="js/bootstrap.js"></script>
 
 	<link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
 	<script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
@@ -68,6 +68,22 @@
 
 </head>
 <body>
+<?php
+// Accès à la BdD
+if(isset($_POST['submit'])) 
+{
+	try {
+$idcom = new PDO("mysql:host=127.0.0.1;dbname=igrilli","root","");
+$idcom->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+		$q =$idcom->exec("INSERT INTO demande(type,etat,delai,emplacement, metier, email, telephone,sujet) VALUES 
+        ('". $_POST['type'] . "',1, " . $_POST['delai'] . ",'" . $_POST['emplacement']. "','" . $_POST['metier'] . "','" . $_POST['email'] ."'," . $_POST['telephone'] .",'" . $_POST['sujet'] . "')"); 
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+    exit;
+}
+	}
+ ?>
+
 	<div class="container-fluid">
 		<div class="row" id="content">
 			<div class="col-md-3" id="k">
@@ -86,43 +102,46 @@
 			<div class="col-md-9" id="liste">
 				<div class="col-md-9 center-block" id="bloc">
 				<form action="" method="POST" role="form">
+				
 					<legend>Demander un service</legend>
 				
 					<div class="form-group">
 						<label for="">email: </label>
-						<input type="email" class="form-control" id="" placeholder="Input field">
+						<input type="email" class="form-control" id="" placeholder="Input field" name="email">
 					</div>
-					<div class="form-group">
+					<div class="form-group" >
 						<label for="">telephone: </label>
-						<input type="tel" class="form-control" id="" placeholder="Input field">
+						<input type="tel" class="form-control" id="" placeholder="Input field" name="telephone">
 					</div>
 					<div class="form-group">
 						<label for="">metier: </label>
-						<select>
+						<select name="metier">
 							<option value="elect">electricien</option>
   							<option value="mecan">mecanicien</option>
 						</select>
 					</div>
 					<div class="form-group">
-					<label for="">urgence</label>
-						<input checked data-toggle="toggle" onchange="myFunction3()" type="checkbox" id="urgence" >
+					<label for="" name="urgence">urgence</label>
+						<input checked data-toggle="toggle" onchange="myFunction3()" type="checkbox" id="urgence" name="type">
 					</div>
-					<div class="form-group" id="delai">
+					<div class="form-group" id="delai" >
 					<label for="">delais par jours</label>
-						<input type="number" name="points" min="0" max="30" step="1" value="0" >
+						<input type="number" name="delai" min="0" max="30" step="1" value="0" >
 					</div>
 					<div class="form-group">
 					<label>sujet: </label>
-					<input type="sujet" class="form-control" id="sujet" placeholder="Enter sujet">
+					<input type="sujet" class="form-control" id="sujet" placeholder="Enter sujet" name="sujet">
+					</div>
+					<div class="form-group">
+					<label>emplacement: </label>
+					<input type="emplacement" class="form-control" id="emplacement" name="emplacement"placeholder="enter emplacement">
 					</div>
 
-					<div class="form-group">
-					<label>Emplacement: </label>
-					<input type="text" class="form-control" id="text" placeholder="Enter la ville">
-					</div>
+				
 					
 				
-					<button type="submit" class="btn btn-primary">Ijrili</button>
+					<button type="submit" class="btn btn-primary"name="submit">Submit</button>
+					
 				</form>
 				</div>
 			</div>
